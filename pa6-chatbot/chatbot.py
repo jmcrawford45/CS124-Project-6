28ab18d239d07aa1fbd10f849fae9b6d7ecb2676
jmcrawford45 <jared13@stanford.edu>
2017-03-01 21:11:45 -0800

    Added non-binary in turbo mode

--- pa6-chatbot/chatbot.py
+++ pa6-chatbot/chatbot.py
@@ -37,9 +37,7 @@ class Chatbot:
         self.intensifiers = set([self.stemmer.stem(line.strip()) for line in f3])
       self.alphanum = re.compile('[^a-zA-Z0-9]')
       self.similarities = {}
-      if not self.is_turbo:
-        self.binarize()
-        self.ratings = self.binaryRatings
+      self.binarize()
       self.recommendations = []
 
 
@@ -107,46 +105,8 @@ class Chatbot:
       # stem words
       input = [self.stemmer.stem(xx) for xx in input]
       input = ' '.join(input)
-      response = ''
-      if self.is_turbo == False:
-        if len(self.recommendations) == 0:
-          if len(movies) == 0:
-            return 'I want to hear more about movies! Tell me about another movie you have seen.'
-          if len(movies) > 1:
-            return 'Please tell me about one movie at a time. Go ahead.'
-          m = movies[0]
-          minDistance = 2 * len(m.split()) #Allow two errors per word
-          spellCorrectedMovie = None
-          if m not in self.titleIndex:
-            for entry in self.titleIndex:
-              distance = self.editDistance(m.lower(), entry)
-              if distance < minDistance:
-                minDistance = distance
-                spellCorrectedMovie = entry
-                print entry, distance
-          if spellCorrectedMovie: m = spellCorrectedMovie
-          print m, minDistance
-          movie = self.remove_articles(m)
-          sentimentScore = self.scoreSentiment(input)
-          if sentimentScore > 0.5:
-            response += 'You liked "%s". Thank you!' % movie
-          elif sentimentScore < -0.5:
-            response += 'You did not like "%s". Thank you!' % movie
-          else:
-            response += 'I\'m sorry, I\'m not quite sure if you liked "%s". Tell me more about "%s".' % (movie, movie)
-          if movie in self.titleIndex:
-            self.userVector[self.titleIndex[movie]] = sentimentScore
-          else:
-            response = 'Hmm, I\'ve never heard of that movie.'
-          self.recommendations = self.recommend(self.userVector)
-          if len(self.recommendations) == 0:
-            return response + ' Tell me about another movie you have seen.'
-        if len(self.recommendations) > 0:
-          response += ('That\'s enough for me to make a recommendation.\n'
-           'I suggest you watch "%s".\n'
-           'Would you like to hear another recommendation? (Or enter :quit if you\'re done.)') % self.recommendations[0]
-          del self.recommendations[0]
-          return response
+      if self.is_turbo == False: return self.starterProcess(movies, input)
+      else: return self.turboProcess(movies, input)
 
 
 
@@ -154,12 +114,79 @@ class Chatbot:
     # 3. Movie Recommendation helper functions                                  #
     #############################################################################
 
+    def turboProcess(self, movies, input):
+      response = ''
+      if len(self.recommendations) == 0:
+        if len(movies) == 0:
+          return 'I want to hear more about movies! Tell me about another movie you have seen.'
+        if len(movies) > 1:
+          return 'Please tell me about one movie at a time. Go ahead.'
+        movie = self.remove_articles(movies[0])
+        minDistance = 3 * len(movie.split()) #Allow three errors per word
+        spellCorrectedMovie = None
+        if movie not in self.titleIndex:
+          for entry in self.titleIndex:
+            distance = self.editDistance(movie.lower(), entry)
+            if entry == 'toy story': print distance, entry
+            if distance < minDistance:
+              minDistance = distance
+              spellCorrectedMovie = entry
+        if spellCorrectedMovie: movie = spellCorrectedMovie
+        sentimentScore = self.scoreSentiment(input)
+        if sentimentScore > 0.5:
+          response += 'You liked "%s". Thank you!' % movie
+        elif sentimentScore < -0.5:
+          response += 'You did not like "%s". Thank you!' % movie
+        else:
+          response += 'I\'m sorry, I\'m not quite sure if you liked "%s". Tell me more about "%s".' % (movie, movie)
+        if movie in self.titleIndex:
+          self.userVector[self.titleIndex[movie]] = sentimentScore
+        else:
+          response = 'Hmm, I\'ve never heard of that movie.'
+        self.recommendations = self.recommend(self.userVector)
+        if len(self.recommendations) == 0:
+          return response + ' Tell me about another movie you have seen.'
+      if len(self.recommendations) > 0:
+        response += (' That\'s enough for me to make a recommendation.\n'
+         'I suggest you watch "%s".\n'
+         'Would you like to hear another recommendation? (Or enter :quit if you\'re done.)') % self.recommendations[0]
+        del self.recommendations[0]
+      return response
+
+    def starterProcess(self, movies, input):
+      response = ''
+      if len(self.recommendations) == 0:
+        if len(movies) == 0:
+          return 'I want to hear more about movies! Tell me about another movie you have seen.'
+        if len(movies) > 1:
+          return 'Please tell me about one movie at a time. Go ahead.'
+        movie = self.remove_articles(movies[0])
+        sentimentScore = self.scoreSentiment(input)
+        if sentimentScore > 0.5:
+          response += 'You liked "%s". Thank you!' % movie
+        elif sentimentScore < -0.5:
+          response += 'You did not like "%s". Thank you!' % movie
+        else:
+          response += 'I\'m sorry, I\'m not quite sure if you liked "%s". Tell me more about "%s".' % (movie, movie)
+        if movie in self.titleIndex:
+          self.userVector[self.titleIndex[movie]] = sentimentScore
+        else:
+          response = 'Hmm, I\'ve never heard of that movie.'
+        self.recommendations = self.recommend(self.userVector)
+        if len(self.recommendations) == 0:
+          return response + ' Tell me about another movie you have seen.'
+      if len(self.recommendations) > 0:
+        response += ('That\'s enough for me to make a recommendation.\n'
+         'I suggest you watch "%s".\n'
+         'Would you like to hear another recommendation? (Or enter :quit if you\'re done.)') % self.recommendations[0]
+        del self.recommendations[0]
+      return response
     
     def extractTitles(self, userInput):
       movies = [m.group(1) for m in re.finditer('"([^"]*)"', userInput)]
       if movies: return movies
       movies = []
-      if self.is_turbo:
+      if self.is_turbo == True:
         tokens = [w.strip() for w in userInput.split() if w.strip() != '']
         i = 0
         bestMatch = ''
@@ -247,11 +274,18 @@ class Chatbot:
       elif (j,i) in self.similarities:
         return self.similarities[(j,i)]
       else:
+        if self.is_turbo == True:
+          num = np.dot(self.ratings[i],self.ratings[j])
+          norm1 = np.linalg.norm(self.ratings[i]+1e-7)
+          norm2 = np.linalg.norm(self.ratings[j]+1e-7)
+          self.similarities[(i,j)] = num/(norm1*norm2)
+          return self.similarities[(i,j)]
         num = np.dot(self.ratings[i],self.binaryRatings[j])
-        norm1 = np.linalg.norm(self.ratings[i]+1e-7)
-        norm2 = np.linalg.norm(self.ratings[j]+1e-7)
+        norm1 = np.linalg.norm(self.binaryRatings[i]+1e-7)
+        norm2 = np.linalg.norm(self.binaryRatings[j]+1e-7)
         self.similarities[(i,j)] = num/(norm1*norm2)
         return self.similarities[(i,j)]
+
 
     def recommend(self, u):
       """Generates a list of movies based on the input vector u using
@@ -260,11 +294,15 @@ class Chatbot:
       for i in range(len(recommendArr)):
         if i not in u:
           recommendValue = 0
+          simSum = 0
           for (movie, rating) in u.items():
             simScore = self.getSimilarity(i,movie)
             if simScore > 0:
               recommendValue += rating*simScore
-          recommendArr[i] = (i, recommendValue)
+              simSum += simScore
+          if self.is_turbo == True and simSum > 0 and recommendValue > 0:
+              recommendArr[i] = (i, recommendValue/simSum)
+          else: recommendArr[i] = (i, recommendValue)
       recommendations = sorted(recommendArr, reverse=True, key = lambda x: x[1])
       return [self.titles[rec[0]][0] for rec in recommendations]
 
